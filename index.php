5c9fbba7dc27d61cac6b601ebd1addaa585c6ddc
Vincenzo <megnavincenzo97@gmail.com>
2020-09-07 08:09:32 +0200

    First Commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title>ajax-dischi</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Lista</h1>
+    </div>
+
+    <!-- TEMPLATE -->
+    <script id="entry-template" type="text/x-handlebars-template">
+        <div class="disco">
+
+        </div>
+    </script>
+
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.2/handlebars.min.js"></script>
+    <script src="script.js" charset="utf-8"></script>
+</body>
+</html>
